61476f162f3bc590df94f0b7dcb9659671999838
Dssaenz <darwinsaenz105@gmail.com>
2021-01-22 16:14:04 -0500

    feat(src): add google maps

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Layout from './Layout';
 import Header from './Header';
 import Footer from './Footer';
 import Product from './Product';
+import Map from './Map';
 
-export { Layout, Header, Footer, Product };
+export { Layout, Header, Footer, Product, Map };
