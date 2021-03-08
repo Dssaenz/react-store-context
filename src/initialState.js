b9dfff5a6a6c9bde2f2b174ae1ef91fb27a82861
Dssaenz <darwinsaenz105@gmail.com>
2021-03-08 13:23:06 -0500

    feat(adjust in context): context

--- src/initialState.js
+++ src/initialState.js
@@ -5,42 +5,48 @@ export default {
   products: [
     {
       id: '0',
-      image: 'https://arepa.s3.amazonaws.com/camiseta.png',
+      image:
+        'https://http2.mlstatic.com/camiseta-para-adulto-con-el-logotipo-de-quinto-sol-nasa-D_NQ_NP_889216-MCO43685300098_102020-F.jpg',
       title: 'Camiseta',
       price: 25,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
       id: '1',
-      image: 'https://arepa.s3.amazonaws.com/mug.png',
+      image:
+        'https://target.scene7.com/is/image/Target/GUEST_616b2da5-25a6-4e52-965e-999ea22e3b32?wid=488&hei=488&fmt=pjpeg',
       title: 'Mug',
       price: 10,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
       id: '2',
-      image: 'https://arepa.s3.amazonaws.com/pin.png',
+      image:
+        'https://cdn11.bigcommerce.com/s-k77di/images/stencil/1280x1280/products/2502/9435/pin_nasa_logo__37734.1588179223.jpg?c=2',
       title: 'Pin',
       price: 4,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
       id: '3',
-      image: 'https://arepa.s3.amazonaws.com/stickers1.png',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/I/41WFrP2gloL._AC_UL600_SR600,600_.jpg',
       title: 'Stickers',
       price: 2,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
       id: '4',
-      image: 'https://arepa.s3.amazonaws.com/stickers2.png',
+      image:
+        'https://i.pinimg.com/originals/b8/9c/60/b89c609d0a8c925ddf28ee663b68983c.jpg',
       title: 'Stickers',
       price: 2,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
       id: '5',
-      image: 'https://arepa.s3.amazonaws.com/hoodie.png',
+      image:
+        'https://apollo-virginia.akamaized.net/v1/files/lbha5l37ailh2-CO/image;s=850x0',
       title: 'Hoodie',
       price: 35,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
